f973dd8b2e221b76dfe87ca248e4f510fd90e455
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-11-01 02:21:20 -0300

    continue on landing page

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,12 @@ function scrollFunction() {
     document.getElementById("navbar").classList.remove('theme');
     document.getElementById("navbar").classList.add('bg-white');
     document.getElementById("navbar").classList.add('scrolled');
-  
+    document.getElementById("logo").classList.remove('logo');
+    document.getElementById("logo").classList.add('logo-scrolled');
   } else{
     document.getElementById("navbar").classList.remove('bg-white');
     document.getElementById("navbar").classList.add('theme');
+    document.getElementById("logo").classList.add('logo');
+    document.getElementById("logo").classList.remove('logo-scrolled');
   }
 }
