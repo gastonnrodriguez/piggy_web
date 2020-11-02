9d4bacc837f7ae239498e561389b2c145d9725b4
gastonnrodriguez <gastonnrodriguez@gmail.com>
2020-11-02 00:43:05 -0300

    added login

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").classList.remove('theme');
-    document.getElementById("navbar").classList.add('bg-white');
+//    document.getElementById("navbar").classList.add('bg-white');
     document.getElementById("navbar").classList.add('scrolled');
     document.getElementById("logo").classList.remove('logo');
     document.getElementById("logo").classList.add('logo-scrolled');
   } else{
-    document.getElementById("navbar").classList.remove('bg-white');
+//    document.getElementById("navbar").classList.remove('bg-white');
+    document.getElementById("navbar").classList.remove('scrolled');
     document.getElementById("navbar").classList.add('theme');
     document.getElementById("logo").classList.add('logo');
     document.getElementById("logo").classList.remove('logo-scrolled');
